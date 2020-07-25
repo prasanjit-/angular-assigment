b41ca9bd71c8c5b21b5492ec0645a6157c99a265
Prasanjit Singh <mailprasanjit@gmail.com>
2020-07-25 14:27:00 +0400

    Create publish-gh-pages.sh

---
+++ publish-gh-pages.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+ng build -bh angular-assigment --aot -prod 
+cd dist/;
+git init;
+git config user.name "prasanjit-" && git config user.email "mailprasanjit@gmail.com";
+git add .;
+git commit -m "github page code"
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
