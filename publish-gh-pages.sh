e4e6ec1ac2b5fb49b408566d56a8579fedf1f49e
Prasanjit Singh <mailprasanjit@gmail.com>
2020-07-25 15:59:16 +0400

    Delete publish-gh-pages.sh

--- publish-gh-pages.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-ng build -bh angular-assigment -prod 
-cd dist/;
-git init;
-git config user.name "prasanjit-" && git config user.email "mailprasanjit@gmail.com";
-git add .;
-git commit -m "github page code"
-git push --force --quiet "https://${GITHUB_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
