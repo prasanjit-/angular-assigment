e94486a2e99e8a6486201ac3eccd6a97fcf1ea43
Prasanjit Singh <mailprasanjit@gmail.com>
2020-07-26 22:25:11 +0400

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist/angular-sapient'));
