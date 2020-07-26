4f4d4e80eccc9b92ac7aae8a56a71c39f40c7d8f
Prasanjit Singh <mailprasanjit@gmail.com>
2020-07-27 00:08:24 +0400

    Update server.js

--- server.js
+++ server.js
@@ -6,25 +6,6 @@ const cors = require('cors');
 const app = express();
 
 
-// add this code
-const whitelist = ['http://localhost:3000']; // list of allow domain
-
-const corsOptions = {
-    origin: function (origin, callback) {
-        if (!origin) {
-            return callback(null, true);
-        }
-
-        if (whitelist.indexOf(origin) === -1) {
-            var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    }
-}
-
-// end 
 
 app.use(cors(corsOptions));
 /////////////////////////////
